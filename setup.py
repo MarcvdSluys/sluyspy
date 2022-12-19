ac5cfb29a069e1161660c0889e57d2f63db3840d
MvdS <git@vandersluys.nl>
2022-12-19 11:21:58 +0100

    Release v0.0.5.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 
 
 # Package version:
-version='0.0.4'
+version='0.0.5'
 
 # Get long description from README.md:
 with open('README.md', 'r') as fh:
@@ -20,7 +20,7 @@ setup(
     url='https://github.com/MarcvdSluys/sluyspy',
     
     packages=['sluyspy'],
-    install_requires=['getch','matplotlib','termcolor'],
+    install_requires=['getch','matplotlib','numpy','termcolor'],
     long_description=long_description,
     long_description_content_type='text/markdown',
     
