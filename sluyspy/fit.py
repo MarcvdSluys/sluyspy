276587230f891363e646d6c64ea0f8d7e0b90664
MvdS <git@vandersluys.nl>
2023-08-19 08:25:37 +0200

    fit/print_fit_details(): print mean/median of signed differences.
    * fit/print_fit_details(): print mean/median of signed differences:
      + in addition to the means and medians of the absolute values of the differences.

--- sluyspy/fit.py
+++ sluyspy/fit.py
@@ -208,12 +208,18 @@ def print_fit_details(fittype, coefs, xvals,yvals,ysigmas, dcoefs=None, var_cov=
     chi2           = _np.sum(yresids**2)          # Chi^2 - NumPy needed for large numbers
     red_chi2       = chi2/(ndat-ncoefs)           # Reduced chi^2
 
-    # abs_ydiffs     = ydiffs                                 # Absolute differences in y
+    abs_ydiffs     = ydiffs                                 # Absolute differences in y
+    mean_abs_ydiff = _np.nanmean(abs_ydiffs)                # The mean absolute difference between data and fit values
+    med_abs_ydiff  = _np.nanmedian(abs_ydiffs)              # The median absolute difference between data and fit values
+    
     abs_abs_ydiffs     = _np.abs(ydiffs)                    # Absolute values of absolute differences in y
     mean_abs_abs_ydiff = _np.nanmean(abs_abs_ydiffs)        # The mean absolute difference between data and fit values
     med_abs_abs_ydiff  = _np.nanmedian(abs_abs_ydiffs)      # The median absolute difference between data and fit values
     
-    # rel_ydiffs         = ydiffs[yvals!=0]/yvals[yvals!=0]   # Relative differences in y
+    rel_ydiffs         = ydiffs[yvals!=0]/yvals[yvals!=0]   # Relative differences in y
+    mean_rel_ydiff     = _np.nanmean(rel_ydiffs)            # The mean absolute difference between data and fit values
+    med_rel_ydiff      = _np.nanmedian(rel_ydiffs)          # The median absolute difference between data and fit values
+    
     abs_rel_ydiffs     = _np.abs(ydiffs[yvals!=0]/yvals[yvals!=0])  # Absolute values of relative differences in y
     mean_abs_rel_ydiff = _np.nanmean(abs_rel_ydiffs)        # The mean absolute difference between data and fit values
     med_abs_rel_ydiff  = _np.nanmedian(abs_rel_ydiffs)      # The median absolute difference between data and fit values
@@ -255,16 +261,26 @@ def print_fit_details(fittype, coefs, xvals,yvals,ysigmas, dcoefs=None, var_cov=
             if verbosity>1: print()
             print('Mean |absolute difference|:  ', sd(mean_abs_abs_ydiff, sigdig))
             print('Med. |absolute difference|:  ', sd(med_abs_abs_ydiff,  sigdig))
-            if verbosity>2: print('Max. |absolute difference|:  ', sd(max_abs_diff_y, sigdig),
-                                  ' @ x =', sd(max_abs_diff_x, sigdig))
+            
+            if verbosity>2:
+                print('Max. |absolute difference|:  ', sd(max_abs_diff_y, sigdig),
+                      ' @ x =', sd(max_abs_diff_x, sigdig))
+                
+                print('Mean absolute difference:    ', sd(mean_abs_ydiff, sigdig))
+                print('Med. absolute difference:    ', sd(med_abs_ydiff,  sigdig))
             
         if rel_diff or (verbosity>2):
             if verbosity>1: print()
             print('Mean |relative difference|:  ', sd(mean_abs_rel_ydiff, sigdig))
             print('Med. |relative difference|:  ', sd(med_abs_rel_ydiff,  sigdig))
-            if verbosity>2: print('Max. |relative difference|:  ', sd(max_rel_diff_y, sigdig),
-                                  ' @ x =', sd(max_rel_diff_x, sigdig))
-        
+            
+            if verbosity>2:
+                print('Max. |relative difference|:  ', sd(max_rel_diff_y, sigdig),
+                      ' @ x =', sd(max_rel_diff_x, sigdig))
+                
+                print('Mean relative difference:    ', sd(mean_rel_ydiff, sigdig))
+                print('Med. relative difference:    ', sd(med_rel_ydiff,  sigdig))
+            
         
         # Print fit coefficients:
         if (verbosity>1) and (coefs is not None):
