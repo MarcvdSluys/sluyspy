39732ad06b7cde5eebfe2bc531b7cf6bef4139a8
MvdS <git@vandersluys.nl>
2023-12-03 17:52:49 +0100

    ephem/horizons_ephem(): jd -> epochs.
    * ephem/horizons_ephem(): jd -> epochs:
      + can't use a long list of JDs in API URL;
      + use start/stop/step dict instead.

--- sluyspy/ephem.py
+++ sluyspy/ephem.py
@@ -17,44 +17,44 @@
 """Ephemerides functions for the sluyspy package"""
 
 
-import pandas as _pd
 from astroquery.jplhorizons import Horizons as _Horizons
 
 
-def horizons_ephem(obj, jd, loc='500@399'):
+def horizons_ephem(obj, epochs, loc='500@399'):
     """Return a Pandas DataFrame with ephemerides from the NASA JPL Horizons system, with my selection of
     variables and column names.
     
     Parameters:
-      obj (str):   Object using Horizons definitions e.g. 199-899 for the centres of the planets 1-8.
-      jd (float):  Julian day for ephemerides.
-      loc (str):   Horizons string for observer location.  Optional, defaults to '500@399' == Geocentre.
+      obj (str):            Object using Horizons definitions e.g. 199-899 for the centres of the planets 1-8.
+      epochs (float/dict):  List of JDs (<~50!) or start/stop/step dict for ephemeride epochs, e.g. Julian days:
+                            start/stop/step dict: e.g. {'start':'2010-01-01', 'stop':'2010-01-10', 'step':'1d'}
+      loc (str):            Horizons string for observer location.  Optional, defaults to '500@399' == Geocentre.
     
     Returns:
       (pd.df):  Pandas DataFrame containing planet ephemerides data.
     """
     
-    # hz = _Horizons(id='Ceres', location='568', epochs={'start':'2010-01-01', 'stop':'2010-01-10', 'step':'1d'})
-    hz = _Horizons(id=obj, location=loc, epochs=jd)  # Return only quantities of interest
+    hz = _Horizons(id=obj, location=loc, epochs=epochs)  # Return only quantities of interest
     
     eph = hz.ephemerides(quantities='30, 18,19, 31,20,39, 1,2,36, 43,10,11,9', extra_precision=True)  # 45 gives error?
-    # optional_settings={'suppress_range_rate':'yes'})
-    # optional_settings: dict, optional: key-value based dictionary to inject some additional optional settings See https://ssd.jpl.nasa.gov/horizons/app.html; default: empty
-    
-    # print(eph.columns)
-    # print(eph)
+    # optional_settings={'suppress_range_rate':'yes'})  # Documented, but gives error?
+    # optional_settings: dict, optional: key-value based dictionary to inject some additional optional settings - see https://ssd.jpl.nasa.gov/horizons/app.html; default: empty
     
     # Convert astropy Table to Pandas DataFrame:
     df = eph.to_pandas()
     
     # Drop unwanted columns:
-    df = df.drop(['targetname','solar_presence','flags','r_rate','delta_rate','r_3sigma','r_rate_3sigma','RA_3sigma','DEC_3sigma','PABLon','PABLat','surfbright'], axis=1)
+    df = df.drop(['targetname','solar_presence','flags','r_rate','delta_rate','r_3sigma','r_rate_3sigma',
+                  'RA_3sigma','DEC_3sigma','PABLon','PABLat','surfbright'], axis=1)
     
     # Rename columns:
-    df = df.rename(columns={'datetime_str':'dtm','datetime_jd':'jd','TDB-UT':'deltat','EclLon':'hc_lon','EclLat':'hc_lat','r':'hc_rad','ObsEclLon':'gc_lon','ObsEclLat':'gc_lat','delta':'gc_rad','RA':'ra','DEC':'dec','RA_app':'ra_app','DEC_app':'dec_app','alpha_true':'phang','illumination':'illum','illum_defect':'illum_def','V':'mag'})  # Rename columns
+    df = df.rename(columns={'datetime_str':'dtm','datetime_jd':'jd','TDB-UT':'deltat','EclLon':'hc_lon',
+                            'EclLat':'hc_lat','r':'hc_rad','ObsEclLon':'gc_lon','ObsEclLat':'gc_lat',
+                            'delta':'gc_rad','RA':'ra','DEC':'dec','RA_app':'ra_app','DEC_app':'dec_app',
+                            'alpha_true':'phang','illumination':'illum','illum_defect':'illum_def','V':'mag'})
     
-    # Convert datetime to datetime:
-    # df.dtm = _pd.to_datetime(df.dtm)  #  - only a limited range!
+    # Convert datetime to "datetime":
+    # df.dtm = _pd.to_datetime(df.dtm)  # Only allowed for a limited range!
     df.dtm = df.dtm.str.replace('-Jan-','-01-')
     df.dtm = df.dtm.str.replace('-Feb-','-02-')
     df.dtm = df.dtm.str.replace('-Mar-','-03-')
@@ -67,6 +67,5 @@ def horizons_ephem(obj, jd, loc='500@399'):
     df.dtm = df.dtm.str.replace('-Oct-','-10-')
     df.dtm = df.dtm.str.replace('-Nov-','-11-')
     df.dtm = df.dtm.str.replace('-Dec-','-12-')
-    # print(df)
     
     return df
