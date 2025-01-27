179331e1c71dde5cd7f94ce2c2a5d9c52c471946
MvdS <git@vandersluys.nl>
2025-01-27 09:12:12 +0100

    system: add functions on_think(), on_hwc() and where_am_i().
    * system: add functions on_think(), on_hwc() and where_am_i():
      + to test on which system I am running.

--- sluyspy/system.py
+++ sluyspy/system.py
@@ -44,14 +44,52 @@ def homedir():
     return str(_Path.home())
 
 
+def on_think():
+    """Test whether I am on a host called think.
+    
+    Returns:
+      (bool):  True when I am on a host called think, False otherwise.
+    """
+    
+    return host() == 'think'
+
+
 def on_zotac():
-    """Tell whether I am on a host called zotac.
+    """Test whether I am on a host called zotac.
     
     Returns:
       (bool):  True when I am on a host called zotac, False otherwise.
     """
     
     return host() == 'zotac'
+
+
+def on_hwc():
+    """Test whether I am on host hwc.
+    
+    Returns:
+      (bool):  True when I am on host hwc, False otherwise.
+    """
+    
+    return _Path('/home/hemel/').is_dir()
+
+
+def where_am_i():
+    """Return on which of a hardcoded selection of hosts I am running, or Unknown.
+    
+    Returns:
+      (str):  Code for the host: think, zotac or hwc.
+    """
+    
+    host = 'Unknown'
+    if on_think:
+        host = 'think'
+    elif on_zotac:
+        host = 'zotac'
+    elif on_hwc:
+        host = 'hwc'
+    
+    return host
 
 
 def temp_file_name(dir_name=None, base_name='.tmpfile', ext='tmp'):
@@ -92,13 +130,17 @@ def tail_file(in_file, out_file, num_lines):
 
 
 def string_in_file(infile, string):
-    """Use the grep command in a shell to quickly check whether a file contains a string.
+    """Check whether a file contains a string.
     
     Parameters:
       infile (str):  Name of the file.
       string (str):  Match string.
     
-    Note: fast small files (<~1Mb) and/or few calls (<~100kb).
+    Returns:
+      (bool):  True if the file contains the string, False otherwise
+    
+    Note: fast small files (<~1Mb) and/or few calls (<~100);
+          - up to 4-6x faster than string_in_file_grep() for a single call.
     """
     
     with open(infile) as f:
@@ -117,7 +159,10 @@ def string_in_file_grep(infile, string):
       infile (str):  Name of the file.
       string (str):  Match string.
     
-    Note: fast for large files (>~1Mb) and/or multiple calls (>~100kb).
+    Returns:
+      (bool):  True if the file contains the string, False otherwise
+    
+    Note: fast for large files (>~1Mb) and/or multiple calls (>~100).
     """
     
     try:
